89ec1df22d3f6984250d3592dc32a6b61064602a
beazt123 <lohzhixuan@live.com>
2021-11-14 21:20:44 +0800

    Hot fix: Add default argument for News Receiver

--- lib/commands/getNews.py
+++ lib/commands/getNews.py
@@ -4,6 +4,8 @@ class GetNews(Command):
     def __init__(self, news) -> None:
         self.receiver = news
 
-    def __call__(self, numArticles = 5):
+    def __call__(self, numArticles):
+        if not numArticles:
+            numArticles = 5
         return self.receiver.getNews(numArticles)
         
