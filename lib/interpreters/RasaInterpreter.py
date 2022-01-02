484dfccb8a5b4ab487e92e301da83ce0f620cdab
beazt123 <lohzhixuan@live.com>
2022-01-02 14:33:49 +0800

    Create readme for each interpreter

--- lib/interpreters/RasaInterpreter.py
+++ lib/interpreters/RasaInterpreter.py
@@ -1,5 +1,6 @@
 import logging
 import requests
+from pvporcupine import KEYWORDS
 from requests.exceptions import Timeout
 from .interpreter import Interpreter
 from lib.commands import (
@@ -18,16 +19,53 @@ from lib.commands import (
 	DefaultCommand
 )
 
+
+README = \
+"""
+J.A.R.V.I.S Intelligence Systems
+================================
+Wake words:
+	Please say any 1 of the following words to wake J.A.R.V.I.S:
+		{wakewords}
+		
+Things you can do with it:
+	1) Search things on Google/Youtube/Wikipedia
+		- For wikipedia, a written response will be shown and read aloud
+		- For Google & Youtube, it will open a browser tab
+
+	2) Play offline music (& stop it after you're done)
+		- Set the music library file path in the config file
+	
+	3) Weather forecast in your area
+		- Set the locale in the config file
+
+	4) News flash
+
+	5) Ask for time
+
+	6) Get it to tell you a programming joke
+
+	7) Define a word
+	
+	8) Shut down the entire computer
+
+""".format(wakewords=", \n\t\t".join(KEYWORDS))
+
 class RasaInterpreterException(Exception):
 	pass
 
 
 
 class RasaInterpreter(Interpreter):
-	RASA_NLU_SERVER_BASE_URL = "localhost:5005/"
+	RASA_NLU_SERVER_BASE_URL = "http://localhost:5005/"
 	RASA_NLU_PARSE_URL = f"{RASA_NLU_SERVER_BASE_URL}model/parse"
 	HEADERS = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+	USER_GUIDE = README
 	logger = logging.getLogger(__name__)
+
+	@classmethod
+	def getUserGuide(cls):
+		return RasaInterpreter.USER_GUIDE
 
 	@staticmethod
 	def mapIntentToEvent(intent):
@@ -72,21 +110,23 @@ class RasaInterpreter(Interpreter):
 
 		return entityPhrase
 
-
-	def interpret(self, command):
+	@classmethod
+	def process(cls, command):
+		if command == Interpreter.FAILED_TOKEN:
+			return DefaultCommand.__name__, None
 		try:
-			response = requests.post(RasaInterpreter.RASA_NLU_PARSE_URL, 
+			response = requests.post(cls.RASA_NLU_PARSE_URL, 
 					json = { "text": command }, 
-					headers = RasaInterpreter.HEADERS,
-					timeout = 2)
+					headers = cls.HEADERS,
+					timeout = 0.7)
 		except Timeout:
 			raise RasaInterpreterException("Server took too long to respond")
 			
 		if response.status_code == 200:
 			js = response.json()
 			intent = js["intent"]["name"]			
-			event = RasaInterpreter.mapIntentToEvent(intent)
-			data = RasaInterpreter.extractEntity(js)
+			event = cls.mapIntentToEvent(intent)
+			data = cls.extractEntity(js)
 
 			return event, data
 
--- lib/interpreters/RegexInterpreter.py
+++ lib/interpreters/RegexInterpreter.py
@@ -16,71 +16,129 @@ from lib.commands import (
 	WikiSearch,
 	DefaultCommand
 )
+from pvporcupine import KEYWORDS
 
+README = \
+"""
+A.I general assistant
+===========================
+Wake words:
+	Please say any 1 of the following words to wake the assistant:
+		{wakewords}
+		
+Command words:
+
+	GOOGLE/YOUTUBE/WIKI: 
+		Performs a search on your browser for Google & Youtube.
+		Wiki searches will show here with audio response.
+		
+		I.e. Say "google most recommended reed diffuser"
+			 Say "youtube bloodstream ed sheeran"
+			 Say "wiki Donald Trump"
+		
+	PLAY / STOP...MUSIC:
+		Plays a random music on a pre-set directory
+		I.e. Say "play some music" or
+			say "play the music" or 
+			say "stop the music" or
+			say "stop playing the music"
+			
+	... WEATHER ...:
+		Gives you a 12 hr weather forecast in 3-hr blocks.
+		I.e. "Weather forecast" or
+			 "How's the weather today?"
+			
+	... NEWS ...:
+		Brings you summaries of the top 5 articles from a specified news site
+		
+	DEFINE <word>:
+		Gives you the dictionary definition of a single word. 
+		Works both offline and online.
+		I.e. "Define apprehensive"
+		
+	TELL...TIME / JOKE:
+		Tells you a joke or the time
+		I.e. "tell me the time" or "tell time"
+			"tell me a joke" or "tell a joke" or even "Tell joke"
+		
+	BYE-BYE:
+		Closes this program.
+		I.e. "goodbye" or "bye" or "bye-bye"
+		
+	SHUTDOWN COMPUTER:
+		Shuts down the computer. Rmb to save your work!
+		I.e. "shut down computer!"
+
+""".format(wakewords=", \n\t\t".join(KEYWORDS))
 
 
 class RegexInterpreter(Interpreter):
 	logger = logging.getLogger(__name__)
+	USER_GUIDE = README
 
+	@classmethod
+	def getUserGuide(cls):
+		return cls.USER_GUIDE
 
-	def interpret(self, command):
+	@classmethod
+	def process(cls, command):
 		event = DefaultCommand.__name__
 		data = None
 		if re.search("^shutdown.*(computer$|system$)", command):
-			RegexInterpreter.logger.info("Detected shutdown command")
+			cls.logger.info("Detected shutdown command")
 			event = ShutdownSystem.__name__
 		elif re.search("(^youtube.*)|(.*(on youtube)$)", command):
-			RegexInterpreter.logger.info("User command contains Youtube")
+			cls.logger.info("User command contains Youtube")
 			if re.search("^play.*", command):
 				command = re.sub("play", "", command)
 			video = re.sub("on youtube", "", command)
 			video = re.sub("youtube", "", video)
-			RegexInterpreter.logger.info(f"video: {video}")
+			cls.logger.info(f"video: {video}")
 			event = PlayYoutubeVideo.__name__
 			data = video
 		elif re.search("^play.*music$", command):
-			RegexInterpreter.logger.info("Detected offline music command: start")
+			cls.logger.info("Detected offline music command: start")
 			event = StartOfflineMusic.__name__
 		elif re.search("^stop.*music$", command):
-			RegexInterpreter.logger.info("Detected offline music command: stop")
+			cls.logger.info("Detected offline music command: stop")
 			event = StopOfflineMusic.__name__
 		elif re.search("((^wiki|^wikipedia).*)|(.*(wiki$|wikipedia$))", command):
-			RegexInterpreter.logger.info(f"Detected wiki search: {command}")
+			cls.logger.info(f"Detected wiki search: {command}")
 			sub_command = command.replace("wikipedia","").strip()
 			search = sub_command.replace("wiki","").strip()
-			RegexInterpreter.logger.info(f"Wiki search statement: {search}")
+			cls.logger.info(f"Wiki search statement: {search}")
 			event = WikiSearch.__name__
 			data = search
 		elif re.search("^google.*", command):
-			RegexInterpreter.logger.info(f"Detected google search")
+			cls.logger.info(f"Detected google search")
 			search = re.sub("google", "", command).strip()
-			RegexInterpreter.logger.info(f"Google search statement: {search}")
+			cls.logger.info(f"Google search statement: {search}")
 			event = GoogleSearch.__name__
 			data = search
 		elif re.search("^define", command):
-			RegexInterpreter.logger.info(f"Detected dictionary command: {command}")
+			cls.logger.info(f"Detected dictionary command: {command}")
 			statement = re.sub("define", "", command).strip().split()
 			lookUpWord = statement[0]
-			RegexInterpreter.logger.info(f"Look up word: {lookUpWord}")
+			cls.logger.info(f"Look up word: {lookUpWord}")
 			event = DefineWord.__name__
 			data = lookUpWord
 		elif re.search("^tell.*", command):
 			if re.search(".*time$", command):
-				RegexInterpreter.logger.info("User is asking for time")
+				cls.logger.info("User is asking for time")
 				event = TellTime.__name__
 			elif re.search(".*joke$", command) or re.search(".*funny$", command):
-				RegexInterpreter.logger.info("User is asking for a joke")
+				cls.logger.info("User is asking for a joke")
 				event = TellAJoke.__name__
 		elif re.search("(^news.*)|(.*news$)", command):
-			RegexInterpreter.logger.info("User is asking for news")
+			cls.logger.info("User is asking for news")
 			event = GetNews.__name__
 		elif "goodbye" in command or "bye" in command or "bye-bye" in command:
-			RegexInterpreter.logger.info("Received command to terminate programme")
+			cls.logger.info("Received command to terminate programme")
 			event = StopProgram.__name__
 		elif re.search("weather", command):
-			RegexInterpreter.logger.info("User is asking for weather forecast")
+			cls.logger.info("User is asking for weather forecast")
 			event = GetWeatherForecast.__name__
 		else:
-			RegexInterpreter.logger.warn("Detected unknown command")
+			cls.logger.warn("Detected unknown command")
 			
 		return event, data
