49b95fe1888d193b8381d792ea13dd7e95e3aefb
beazt123 <lohzhixuan@live.com>
2022-02-06 23:11:55 +0800

    Change main programme into CLI with yaml file as a compulsory argument. Add new yaml files

--- ajax.py
+++ ajax.py
@@ -1,46 +1,30 @@
 
 import sys
 import logging
+import argparse
+from os import path, listdir
+from pathlib import Path
 from logging.handlers import SocketHandler
 from logging import StreamHandler
-from os import path
-from lib.utils.setUp import createApp, getConfig, getSetUpConfig
-
-args = sys.argv[1:]
-kwargs = dict(arg.upper().split('=') for arg in args)
-
-numeric_level = getattr(logging, kwargs.get('LOG', "CRITICAL"), None)
-
-sockethandler = SocketHandler('localhost', 19996)
-streamHandler = StreamHandler(sys.stdout)
-streamHandler.setLevel(numeric_level)
-
-logging.basicConfig(
-	level=logging.NOTSET, 
-	handlers=[
-		sockethandler,
-		streamHandler
-		]
-	)
+from lib.utils.utils import getConfig, getSetUpConfig
+from lib.utils.setUp import createApp
 
 
 
-def main():
+
+def main(path_to_setup_config):
 	path_to_system_config = path.join("lib", "config", "files")
-	path_to_user_config = path.join("lib", "config", "devices", "windowsLaptop")
-	path_to_setup_config = path.join(path_to_user_config, "setup.yaml")
 	system_config_files = [
 		path.join(path_to_system_config, "sysconfig.ini"),
-		path.join(path_to_system_config, "keys.ini"),
-		path.join(path_to_user_config, "userconfig.ini")
-
+		path.join(path_to_system_config, "keys.ini")
 	]
-
-
 	config = getConfig(*system_config_files)
-	setup_config = getSetUpConfig(path_to_setup_config)
-	app = createApp(config, setup_config)
 
+	# path_to_setup_config = path.join("lib", "config", "devices", "windowsLaptop.yaml")
+	setup_config = getSetUpConfig(path_to_setup_config)
+
+
+	app = createApp(config, setup_config)
 	app.run()
 	
 
@@ -48,4 +32,66 @@ def main():
 
 
 if __name__ == "__main__":
-	main()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("config",
+						help="Name of the file without extension if it's in the config/devices folder, else full path",
+						type=str)
+
+	parser.add_argument("--list",
+						action="store_true",
+						help="Displays the list of config files")
+
+	parser.add_argument("-l", "--log",
+						type=str,
+						help="Set the log level")
+
+	args = parser.parse_args()
+	config = args.config
+	ls = args.list
+	if args.log == None:
+		log_level = "CRITICAL"
+	else:
+		log_level = args.log.upper()
+
+
+	numeric_level = getattr(logging, log_level, None)
+	sockethandler = SocketHandler('localhost', 19996)
+	streamHandler = StreamHandler(sys.stdout)
+	streamHandler.setLevel(numeric_level + 10)
+
+	logging.basicConfig(
+		level=logging.NOTSET, 
+		handlers=[
+			sockethandler,
+			streamHandler
+			]
+		)
+
+	pathToConfigFiles = path.join("lib", "config", "devices")
+	allConfigFiles = listdir(pathToConfigFiles)
+	configFiles = list(map(lambda x : Path(x).stem, allConfigFiles))
+	configFiles.remove("__pycache__")
+
+	if path.exists(config):
+		configfilepath = config
+
+	elif config.lower() == "config" and ls:
+		print("The following config files were found:")
+		for file in configFiles:
+			print(f"\t{file}")
+		exit()
+
+	else:
+		try:
+			chosenConfigIndex = configFiles.index(config)
+		except ValueError:
+			print(f"{config} is not found in available configurations. The following files were found instead:")
+			for file in configFiles:
+				print(file)
+			exit()
+		
+		configfilepath = path.join(pathToConfigFiles, allConfigFiles[chosenConfigIndex])
+	# print("Running main...")
+	# print(configfilepath)
+
+	main(configfilepath)
