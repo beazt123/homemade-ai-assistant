8a2bdcf01a410f24da78ca7a2fce7effec57765f
beazt123 <lohzhixuan@live.com>
2022-02-06 23:00:28 +0800

    Add selected topics as a constructor parameter to IOTClient class

--- lib/iot/IOTClient.py
+++ lib/iot/IOTClient.py
@@ -1,7 +1,21 @@
 
 from abc import ABC, abstractmethod
+from ..utils.utils import getSetUpConfig
+from os.path import join
 
 class IOTClient(ABC):
+    ALLOWED_COMMANDS = getSetUpConfig(join("lib",
+                                            "commands",
+                                            "iotCommands.yaml"))
+
+    def __init__(self, chosen_commands) -> None:
+        chosen_commands = set(chosen_commands)
+        allowed_cmds = set(IOTClient.ALLOWED_COMMANDS.keys())
+        if not chosen_commands.issubset(allowed_cmds):
+            raise ValueError(f"{chosen_commands.difference(allowed_cmds)} are not allowed")
+        else:
+            self.chosen_commands = chosen_commands
+
     @abstractmethod
     def publish(self, topic, msg):
         pass
